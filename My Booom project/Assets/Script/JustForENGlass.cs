a17c6e7970101e93f3330f4ae84bbadb97731b95
aidendenden233 <2996876880@qq.com>
2023-08-25 04:07:07 +0800

    小修改

--- My Booom project/Assets/Script/JustForENGlass.cs
+++ My Booom project/Assets/Script/JustForENGlass.cs
@@ -5,15 +5,25 @@ using UnityEngine;
 public class JustForENGlass : MonoBehaviour
 {
     public Animator animator;
-    public AudioSource audioSource;
+    public AudioSource audioSource1;
+    public AudioSource audioSource2;
+
+    private bool _b;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "playerGalss")
         {
-           
             animator.SetTrigger("Change");
-            audioSource.PlayOneShot(audioSource.clip);
-            Debug.Log("aa");
+            if (_b)
+            {
+                audioSource1.PlayOneShot(audioSource1.clip);
+            }
+            else
+            {
+                audioSource2.PlayOneShot(audioSource2.clip);
+            }
+            _b = !_b;
+            //Debug.Log("aa");
         }
         Debug.Log(collision.gameObject.tag);
     }
